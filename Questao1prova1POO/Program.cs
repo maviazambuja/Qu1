6f9e63b6a328bcdd411df9d37d4fe39fa3aa4972
Maria Vitória Myron <Maria Vitria Myron@LAPTOP-J2Q0MIHD>
2023-09-18 11:37:03 -0300

    Add project files.

---
+++ Questao1prova1POO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1prova1POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
